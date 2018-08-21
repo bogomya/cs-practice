0d8e52651a4a70120356e703e023cce080f014b4
Dmitry Bogomya <dmitry.bogomya@gmail.com>
2018-08-21 22:25:26 +0300

    Optimize coverByPoints

--- algorithms/greedy/coverByPoints/coverByPoints.py
+++ algorithms/greedy/coverByPoints/coverByPoints.py
@@ -2,12 +2,8 @@ def coverByPoints(segments: (int, int)):
     points = []
     orderedSegments = sorted(segments, key=lambda segment: segment[1])
     for i, segment in enumerate(orderedSegments):
-        lastPoint = points[-1] if points else 0
-        isLastSegment = i == len(orderedSegments) - 1
-        if isLastSegment and lastPoint > segment[0]:
-            continue
-
-        if segment[1] > lastPoint:
+        lastPoint = points[-1] if points else -1
+        if segment[0] > lastPoint:
             points.append(segment[1])
 
     return points
@@ -20,6 +16,7 @@ def main():
         begin, end = map(lambda i: int(i), input().split(" "))
         segments.append(( begin, end))
     points = coverByPoints(segments)
+    print(len(points))
     print(" ".join(map(str, points)))
 
 if __name__ == "__main__":
