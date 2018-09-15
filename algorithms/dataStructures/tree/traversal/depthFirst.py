0ec9c5444bcaeb98a762db92c75f6b7a61396760
Dmitry Bogomya <dmitry.bogomya@gmail.com>
2018-09-15 23:37:49 +0300

    Pre-order traversal in python

--- algorithms/dataStructures/tree/traversal/depthFirst.py
+++ algorithms/dataStructures/tree/traversal/depthFirst.py
@@ -1,8 +1,15 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from binarySearchTree import Node, initTree
+from binarySearchTree import Node
 
 class TraversedNode(Node):
+    def preOrderTraverse(self):
+        print(self.data)
+        if self.left:
+            self.left.preOrderTraverse()
+        if self.right:
+            self.right.preOrderTraverse()
+
     def inOrderTraverse(self):
         if self.left:
             self.left.inOrderTraverse()
@@ -10,7 +17,41 @@ class TraversedNode(Node):
         if self.right:
             self.right.inOrderTraverse()
 
+def initTree(nodeCreator):
+    A = nodeCreator("A")
+    B = nodeCreator("B")
+    C = nodeCreator("C")
+    D = nodeCreator("D")
+    E = nodeCreator("E")
+    K = nodeCreator("K")
+    L = nodeCreator("L")
+    M = nodeCreator("M")
+    N = nodeCreator("N")
+    O = nodeCreator("O")
+    P = nodeCreator("P")
+    R = nodeCreator("R")
+    S = nodeCreator("S")
+    A.left = B
+    A.right = M
+    B.left = C
+    B.right = E
+    C.right = D
+    E.left = K
+    E.right = L
+    M.left = N
+    M.right = O
+    O.right = P
+    P.left = R
+    P.right = S
+    return A
+
 if __name__ == '__main__':
     root = initTree(TraversedNode)
+    print("inOrderTraverse")
     root.inOrderTraverse()
+    print("preOrderTraverse")
+    root.preOrderTraverse()
+
+
+
 
