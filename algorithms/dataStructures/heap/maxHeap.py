965d419f14d419cb7d3c66e2944fb968b0b8e27f
Dmitry Bogomya <dmitry.bogomya@gmail.com>
2018-09-17 23:22:52 +0300

    Add max heap base interface

---
+++ algorithms/dataStructures/heap/maxHeap.py
@@ -0,0 +1,27 @@
+class MaxHeap:
+    def insert(self):
+        print("insert")
+    def siftUp(self):
+        print("siftUp")
+
+    def getMax(self):
+        print("getMax")
+    def getSize(self):
+        print("getSize")
+    def isEmpty(self):
+        print("isEmpty")
+
+    def extractMax(self):
+        print("extractMax")
+    def siftDown(self):
+        print("siftDown")
+
+    def remove(self, index):
+        print("Remove")
+
+    #create a heap from an array of elements, needed for heapSort
+    def heapify(self):
+        print("heapify")
+    #take an unsorted array and turn it into a sorted array in-place using a max heap
+    def heapSort(self):
+        print("heapSort")
