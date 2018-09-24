c4107f2db9539d9a1683f0482d1e705269a952d7
Dmitry Bogomya <dmitry.bogomya@gmail.com>
2018-09-24 16:50:16 +0300

    Queue implementation with linked list

--- algorithms/dataStructures/queue/queue.py
+++ algorithms/dataStructures/queue/queue.py
@@ -1,15 +1,36 @@
+import sys
+sys.path.append('../')
+from linkedList.linkedList import SinglyLinkedList
+
 class Queue:
   def __init__(self):
       self.queue = []
 
-  def push(self, dataval):
-      self.queue.insert(0,dataval)
+  def push(self, value):
+      self.queue.insert(0,value)
   def pop(self):
       return self.queue.pop() if len(self.queue) else None;
 
-queue = Queue()
-queue.push("Mon")
-queue.push("Tue")
-queue.push("Wed")
-print(queue.pop())
-print(queue.pop())
+
+class LLQueue:
+    def __init__(self):
+        self.queue = SinglyLinkedList()
+    def enqueue(self, value):
+        self.queue.pushBack(value)
+    def dequeue(self):
+        return self.queue.popFront()
+
+if __name__ == '__main__':
+    queue = Queue()
+    queue.push("Mon")
+    queue.push("Tue")
+    queue.push("Wed")
+    print(queue.pop())
+    print(queue.pop())
+
+    queue = LLQueue()
+    queue.enqueue("Mon")
+    queue.enqueue("Tue")
+    queue.enqueue("Wed")
+    print(queue.dequeue())
+    print(queue.dequeue())
