b0ad5ee7df8ae2bc5f5ccbbade651f88d24f0d89
Dmitry Bogomya <dmitry.bogomya@gmail.com>
2018-11-01 22:20:50 +0200

    Update parity spec

--- puzzles/primitive/parityWord/parity.spec.ts
+++ puzzles/primitive/parityWord/parity.spec.ts
@@ -7,9 +7,23 @@ function simpleParity(value: number) {
     return parity;
 }
 
-describe("simpleParity", () => {
-    it("should return correct parity", () => {
+function erasedFirstSetBitParity(value: number) {
+    let parity = 0;
+    while(value) {
+        parity ^= 1;
+        value = value & (value -1); // clear first from the right set (=1) bit
+    }
+    return parity;
+}
+
+describe("parity", () => {
+    it("simpleParity should return correct parity", () => {
         expect(simpleParity(5)).toEqual(0);// 00000101
         expect(simpleParity(7)).toEqual(1);// 00000111
+    });
+
+    it("erasedFirstSetBitParity should return correct parity", () => {
+        expect(erasedFirstSetBitParity(33)).toEqual(0);// 00100001
+        expect(erasedFirstSetBitParity(32)).toEqual(1);// 00100000
     });
 });
