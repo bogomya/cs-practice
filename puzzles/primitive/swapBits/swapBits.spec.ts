d298e50731a5f477e5e5e7df086f33b794430567
Dmitry Bogomya <dmitry.bogomya@gmail.com>
2018-11-03 14:04:20 +0200

    Swap bits

---
+++ puzzles/primitive/swapBits/swapBits.spec.ts
@@ -0,0 +1,17 @@
+function swapBits(value: number, i: number, j: number) {
+    if ((value & (1 >> i)) !== (value & (1 >> j))) {
+        const mask = (1 << i) | (1 << j);
+        return value ^ mask;
+    }
+    return value;
+}
+
+describe("swapBits", () => {
+    it("should swap bits i and j", () => {
+        expect(swapBits(parseInt("01001001", 2), 1, 6) === parseInt("00001011", 2));
+    });
+
+    it("should return the same number for the same bits i and j", () => {
+        expect(swapBits(parseInt("01001001", 2), 2, 3) === parseInt("01001001", 2));
+    })
+});
